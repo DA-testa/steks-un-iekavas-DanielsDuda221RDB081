0809f6e27c3ddbb56a6ac3e4a6a9a54d4675e583
DanielsDuda221RDB081 <Daniels.Duda@edu.rtu.lv>
2023-02-06 22:18:49 +0200

    try

--- main.py
+++ main.py
@@ -12,34 +12,23 @@ def are_matching(left, right):
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
-        # print(next)
-        # print(opening_brackets_stack)
         if next in "([{":
-            # Process opening bracket, write your code here
             opening_brackets_stack.append(i+1)
             opening_brackets_stack.append(next)
             pass
 
         if next in ")]}":
-            # Process closing bracket, write your code here
             stack_length = opening_brackets_stack.__len__()
             if stack_length == 0:
-                # mismatch
-                # print("exit 1")
                 return i+1
 
             idx = stack_length-1
             last_bracket = opening_brackets_stack[idx]
-            # print("last:"+last_bracket)
             if are_matching(last_bracket, next):
-                # match
                 opening_brackets_stack.pop()
                 opening_brackets_stack.pop()
             else:
-                # mismatch
-                # print("exit 2")
                 return i+1
-            # change2
             pass
 
     if opening_brackets_stack.__len__() > 0:
@@ -52,8 +41,7 @@ def find_mismatch(text):
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    if mismatch < 0:
+    if mismatch < 0 and text.__len__() <= 105:
         print("Success")
     else:
         print(mismatch)
