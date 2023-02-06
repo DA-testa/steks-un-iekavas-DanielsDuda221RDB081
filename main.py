97f34bd1a42af5efc96ec3fa862113e1560e0dea
Dāniels Dūda 221RDB081 <124591745+DanielsDuda221RDB081@users.noreply.github.com>
2023-02-06 22:31:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,38 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
+            opening_brackets_stack.append(i+1)
+            opening_brackets_stack.append(next)
             pass
 
         if next in ")]}":
-            # Process closing bracket, write your code here
+            stack_length = opening_brackets_stack.__len__()
+            if stack_length == 0:
+                return i+1
+
+            idx = stack_length-1
+            last_bracket = opening_brackets_stack[idx]
+            if are_matching(last_bracket, next):
+                opening_brackets_stack.pop()
+                opening_brackets_stack.pop()
+            else:
+                return i+1
             pass
+
+    if opening_brackets_stack.__len__() > 0:
+        opening_brackets_stack.pop()
+        return opening_brackets_stack.pop()
+
+    return -1
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    if mismatch < 0 and text.__len__() <= 105:
+        print("Success")
+    else:
+        print(mismatch)
 
 
 if __name__ == "__main__":
