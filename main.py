7789ae6c151c4685656ba8fa4955c874df14d729
DanielsDuda221RDB081 <Daniels.Duda@edu.rtu.lv>
2023-02-06 20:06:42 +0200

    demo commit 1

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            # change1
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            # change2
             pass
 
 
